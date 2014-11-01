edff901a76fee107a8bb1d56a1a2bf76ec9f2e5e
aguaaguaaguaagua <aguaaguaagua@126.com>
2014-11-01 14:00:42 +0800

    fucking shit

--- hehe/hehe.cpp
+++ hehe/hehe.cpp
@@ -19,7 +19,22 @@ int _tmain(int argc, _TCHAR* argv[])
 	if(!cam.isOpened()){cout<<"file fail being read"<<endl;return -1;}
 	       
 
-	int framecount=0;
+	int framecount=11900;
+	cam.set(CV_CAP_PROP_POS_FRAMES,framecount);
+	cam>>frame;
+	Mat temp;
+	frame.copyTo(temp);
+	for(int i=0;i<temp.rows;i++)
+		for(int j=0;j<temp.cols;j++)
+			{if((temp.at<Vec3b>(i,j)[0]<150)||(temp.at<Vec3b>(i,j)[1]<175)||(temp.at<Vec3b>(i,j)[2]<175))
+				temp.at<Vec3b>(i,j)=Vec<uchar,3>(0,0,0);
+			if((temp.at<Vec3b>(i,j)[0]>250)&&(temp.at<Vec3b>(i,j)[1]>250)&&(temp.at<Vec3b>(i,j)[2]>250))
+				temp.at<Vec3b>(i,j)=Vec<uchar,3>(0,0,0);
+			if(temp.at<Vec3b>(i,j)[0]>temp.at<Vec3b>(i,j)[1])
+				temp.at<Vec3b>(i,j)=Vec<uchar,3>(0,0,0);
+		}
+			
+	imshow("shit",temp);
 #ifdef HistAnalyze
 
 	assert(readFile("F:/FFOutput/line1.txt")==1);
